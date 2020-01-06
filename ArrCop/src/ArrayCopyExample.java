129d9f5708dcd2dc48fa86516cbc176f42d84862
lenovo <lenovo@DESKTOP-O1E1LBE>
2020-01-06 20:31:28 +0530

    File Commit Upload all files

---
+++ ArrCop/src/ArrayCopyExample.java
@@ -0,0 +1,43 @@
+
+public class ArrayCopyExample {
+	public static void main(String[] args) {
+		int s[] = { 10,20,30,40,50,60,70,80,90,100};
+		int d[] = { 15,25,35,45,55,65,75,85,95,105};
+		
+		int source_arr[], sourcePos, des_arr[], destPos, len;
+		source_arr = s;
+		sourcePos = 3;
+		des_arr = d;
+		destPos = 5;
+		len = 4;
+		
+		//Print elements of source
+		System.out.print("source_array : ");
+		for (int i = 0; i < s.length; i++) {
+			System.out.print(s[i] + " ");}
+		System.out.println(" ");
+		
+		System.out.println("sourcePos : " + sourcePos);
+		
+		//Print elements of source
+		System.out.print("des_array  : ");
+		for (int j = 0; j < d.length; j++) {
+			System.out.print(d[j] + " ");	}
+		
+		System.out.println(" ");
+		
+		System.out.println("destPos : " + destPos);
+		
+		System.out.println("len : " + len);
+		
+		//Use of arraycopy()method
+		System.arraycopy(source_arr, sourcePos, des_arr, destPos, len);
+		
+		//Print elements of destination after
+		System.out.print("final dest_array : ");
+		for (int i = 0; i < d.length; i++) {
+			System.out.print(d[i] + " ");}
+	}
+	
+}
+ 
